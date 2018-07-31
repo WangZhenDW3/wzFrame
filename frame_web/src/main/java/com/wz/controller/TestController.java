e8f321e0171cfb3746ffff5a631a795c3610ad02
WangZhenDW3 <312040460@qq.com>
2018-07-31 23:21:59 +0800

    test commit

--- frame_web/src/main/java/com/wz/controller/TestController.java
+++ frame_web/src/main/java/com/wz/controller/TestController.java
@@ -23,9 +23,6 @@ public class TestController {
 
     @RequestMapping("/test3")
     public void test3(){
-        System.out.println("ok");
-        System.out.println("ok33333333333");
-        System.out.println("ok33333333333");
         System.out.println("ok33333");
     }
 }
