306fb896b09b388d2fc223f0428bd8aa7539d1e8
WangZhenDW3 <312040460@qq.com>
2018-08-16 16:50:00 +0800

    test commit

---
+++ frame_web/src/main/java/com/wz/bean/UserTest.java
@@ -0,0 +1,16 @@
+package com.wz.bean;
+
+/**
+ * Created by wangzhen on 2018/8/7.
+ */
+public class UserTest {
+    String json;
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+}
